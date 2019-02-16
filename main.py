5357a530a409f74e2ba827f57fc66f7734ffedf6
Jordy Moos <jordymoos@gmail.com>
2019-02-16 12:42:35 +0100

    Testing color detection

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('./image.png', cv2.IMREAD_COLOR)
+
+hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+lower = np.array([50, 50, 50])
+upper = np.array([70, 255, 255])
+
+# Threshold to only get the color we want
+mask = cv2.inRange(hsv, lower, upper)
+
+# Bitwise-AND mask and original
+res = cv2.bitwise_and(img, img, mask = mask)
+
+cv2.imshow('image',img)
+cv2.imshow('mask', mask)
+cv2.imshow('res', res)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
