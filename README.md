4bdfe361c0ed41658a6c1620f40a0b56b5f87c37
Jordy Moos <jordy@pbwebmedia.com>
2019-02-20 17:25:58 +0100

    Clean up

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pokémon catcher with Raspberry Pi
+
+Little devices that uses your phone and pokeball plus to catch pokemons for you.
+
+## Catching Pokémon
+[![Cathing Pokémon](./docs/images/pokemon-catcher-v0.1.jpg)](https://twitter.com/JordyMoos/status/1097080274402373632)
+
+## Pair devices
+[![Pair devices](./docs/images/pokemon-catcher-v0.2.jpg)](https://twitter.com/JordyMoos/status/1098143762650415104)
--- main.py
+++ main.py
@@ -8,38 +8,56 @@ import RPi.GPIO as GPIO
 import time
 
 # Config
-minimumPixels = 10000
+PIN_PAIR = 27
+PIN_SERVO_PAIR = 12
+PIN_SERVO_POKEBALL = 18
+REPAIR_IDLE_TIME = 60 * 7
+REPAIR_DELAY_TIME = 30
+
+MINIMUM_PIXELS = 10000
+RESOLUTION = (640, 480)
+
+
+def setup_servo(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    servo = GPIO.PWM(pin, 50)
+    servo.start(0.1)
+    time.sleep(0.2)
+    servo.ChangeDutyCycle(0)
+    return servo
+
+
+def trigger_servo(servo):
+    # Turn servo on
+    servo.ChangeDutyCycle(0.1)
+    time.sleep(0.1)
+    # Activate the button
+    servo.ChangeDutyCycle(3.5)
+    time.sleep(0.2)
+    # Go back to the off position
+    servo.ChangeDutyCycle(0.1)
+    time.sleep(0.1)
+    # Turn servo off
+    servo.ChangeDutyCycle(0)
+
 
 # GPIO Setup
-GPIO.setwarnings(False) # Do not tell anyone
+GPIO.setwarnings(False)  # Do not tell anyone
 GPIO.setmode(GPIO.BCM)
 
-# Pair Servo
-pairServoPin = 12
-GPIO.setup(pairServoPin, GPIO.OUT)
-pairServo = GPIO.PWM(pairServoPin, 50)
-pairServo.start(0.1) # "Neutral"
-pairServo.ChangeDutyCycle(0)
-
-# Pokeball Servo
-pokeballServoPin = 18
-GPIO.setup(pokeballServoPin, GPIO.OUT)
-pokeballServo = GPIO.PWM(pokeballServoPin, 50)
-pokeballServo.start(0.1) # "Neutral"
-time.sleep(0.1)
-pokeballServo.ChangeDutyCycle(0)
+# Servo's
+pairServo = setup_servo(PIN_SERVO_PAIR)
+pokeballServo = setup_servo(PIN_SERVO_POKEBALL)
 
 # Pair button
-pairPIN = 27
-GPIO.setup(pairPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_PAIR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Camera
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = RESOLUTION
 camera.framerate = 6
-camera.hflip = True
-rawCapture = PiRGBArray(camera, size=(640, 480))
-time.sleep(1) # Camera needs some time for itself
+rawCapture = PiRGBArray(camera, size=RESOLUTION)
+time.sleep(1)  # Camera needs some time for itself
 
 # Image transformation
 blueLower = np.array([10, 70, 70])
@@ -47,92 +65,55 @@ blueUpper = np.array([30, 255, 255])
 greenLower = np.array([35, 70, 70])
 greenUpper = np.array([70, 255, 255])
 
-
 lastInteraction = time.time() - 1000000
-
-
-def trigger_pokeball():
-    # Turn servo on
-    pokeballServo.ChangeDutyCycle(0.1)
-    time.sleep(0.1)
-    # Activate the button
-    pokeballServo.ChangeDutyCycle(3.5)
-    time.sleep(0.2)
-    # Go back to the off position
-    pokeballServo.ChangeDutyCycle(0.1)
-    time.sleep(0.1)
-    # Turn servo off
-    pokeballServo.ChangeDutyCycle(0)
-
-
-def trigger_pair():
-    # Turn servo on
-    pairServo.ChangeDutyCycle(0.1)
-    time.sleep(0.1)
-    # Activate the button
-    pairServo.ChangeDutyCycle(3.5)
-    time.sleep(0.2)
-    # Go back to the off position
-    pairServo.ChangeDutyCycle(0.1)
-    time.sleep(0.1)
-    # Turn servo off
-    pairServo.ChangeDutyCycle(0)
 
 for rgbFrame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         print("Quit")
         break
-    
+
     hsvFrame = cv2.cvtColor(rgbFrame.array, cv2.COLOR_RGB2HSV)
-    
+
     # Pokestop
     blueMask = cv2.inRange(hsvFrame, blueLower, blueUpper)
     blueCount = cv2.countNonZero(blueMask)
-    
+
     # Pokemon
     greenMask = cv2.inRange(hsvFrame, greenLower, greenUpper)
     greenCount = cv2.countNonZero(greenMask)
-    
-    '''
-    cv2.putText(mask, "%s" %(nonZeroPixels), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-    cv2.imshow("mask", mask)
-    '''
 
-    #res = cv2.bitwise_and(rgbFrame.array, rgbFrame.array, mask = blueMask)
-    #cv2.imshow('rgbFrame', rgbFrame.array)
-    #cv2.imshow('blueMask', blueMask)
-    #cv2.imshow('greenMask', greenMask)
-    #cv2.imshow('res', res)
-    
+    # res = cv2.bitwise_and(rgbFrame.array, rgbFrame.array, mask = blueMask)
+    # cv2.imshow('rgbFrame', rgbFrame.array)
+    # cv2.imshow('blueMask', blueMask)
+    # cv2.imshow('greenMask', greenMask)
+    # cv2.imshow('res', res)
+
     rawCapture.truncate(0)
-    
-    if GPIO.input(pairPIN) == False:
+
+    if not GPIO.input(PIN_PAIR):
         print("Button Pressed")
-        trigger_pair()
-        trigger_pokeball()
+        trigger_servo(pairServo)
+        trigger_servo(pokeballServo)
         time.sleep(1)
-    
-    elif blueCount > minimumPixels and blueCount > greenCount:
+
+    elif blueCount > MINIMUM_PIXELS and blueCount > greenCount:
         lastInteraction = time.time()
         time.sleep(2)
-    
-    elif greenCount > minimumPixels:
+
+    elif greenCount > MINIMUM_PIXELS:
         lastInteraction = time.time()
-        trigger_pokeball()
+        trigger_servo(pokeballServo)
         # Sleep so we do not keep hitting the button
         time.sleep(3)
-    
-    elif lastInteraction < time.time() - 420:
-        trigger_pair()
-        trigger_pokeball()
-        lastInteraction = time.time() - 400
+
+    elif lastInteraction < time.time() - REPAIR_IDLE_TIME:
+        trigger_servo(pairServo)
+        trigger_servo(pokeballServo)
+        lastInteraction = time.time() - REPAIR_IDLE_TIME - REPAIR_DELAY_TIME
         time.sleep(5)
 
-
-
-pokeballServoPin.stop()
+pokeballServo.stop()
 pairServo.stop()
 GPIO.cleanup()
 cv2.destroyAllWindows()
